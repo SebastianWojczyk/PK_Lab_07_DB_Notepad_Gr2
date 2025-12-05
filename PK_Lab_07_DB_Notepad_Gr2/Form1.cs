b6cd19ff243fe91c89d1985278e55a5dace01e07
Student <Student@C120_25>
2025-12-05 10:38:21 +0100

    SQL i instukcja tworzenia bazy.

--- PK_Lab_07_DB_Notepad_Gr2/Form1.cs
+++ PK_Lab_07_DB_Notepad_Gr2/Form1.cs
@@ -7,6 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+/*
+CREATE TABLE[dbo].[Picture]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(50) NOT NULL
+);
+CREATE TABLE[dbo].[Line]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [XStart] INT NOT NULL,
+
+    [YStart] INT NOT NULL,
+
+    [XStop] INT NOT NULL,
+
+    [YStop] INT NOT NULL,
+    [PictureId] INT NOT NULL,
+    CONSTRAINT[FK_Line_Picture] FOREIGN KEY ([PictureId]) REFERENCES[Picture]([Id])
+);
+
+-w Bazie "New query"
+- Add / New item / LINQ to SQL Classes
+- Klikamy Tak - kopiowanie bazy do projektu
+- Przeciągamy tabele z bazy do modelu
+- dla pliku bazy *.mdf zmieniamy "Copy always" na "Copy if newer"
+*/
 
 namespace PK_Lab_07_DB_Notepad_Gr2
 {
